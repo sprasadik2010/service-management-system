c69e074205b0365c66a9f4108a1656868967ecb4
SIVA PRASAD I K <sprasadik@gmail.com>
2025-10-28 21:38:07 +0530

    new update

--- server/database.py
+++ server/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1q2w3e4r@localhost/service_management"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1q2w3e4r@localhost/service_management"
+SQLALCHEMY_DATABASE_URL = 'postgresql://sms2025:gX08v1zqdwqDMH9QxOCFkFjrDnTRzxCX@dpg-d40dp73e5dus738bsung-a/service_management'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
